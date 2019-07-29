2d6204c51f19e999c26b982d4e4ee47623987e9e
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-29 19:51:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object{}
 
-Function updateObjectWithKeyAndValue(object, key, value);
+function updateObjectWithKeyAndValue(object, key, value)
 {
-  return Object.assign( {}, object, {[key]: value});
+  return Object.assign( {}, object, {[key]: value})
 }
